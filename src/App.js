ddc5355284997536709ce3a4a51433580223d39e
Nick Walters <nick.walters@argandsolutions.com>
2021-01-23 17:24:57 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,172 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Helmet } from "react-helmet";
+// import isValidDate from './js/functions.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Form from "react-bootstrap/Form";
+
+import './App.css';
+import db from './firebase.js';
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nameinput: "default",
+      sLat: 0.0,
+      sLong:0.0,
+      error: false,
+      errortxt: " ",
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.writeDataBase = this.writeDataBase.bind(this);
+    this.setPos = this.setPos.bind(this);
+    this.prepareData = this.prepareData.bind(this);
+    this.writeDataBase = this.writeDataBase.bind(this);
+  } 
+
+  handleSubmit(event) {
+    this.writeDataBase();
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({nameinput: event.target.value});
+  }
+  
+  setPos(position) {
+
+    this.setState({errortxt:" "});
+    this.setState({error:true});
+    this.setState({
+      sLat: position.coords.latitude,
+      sLong: position.coords.longitude,
+      errottxt:"",
+    })
+  }
+
+  prepareData(){
+    this.setState({errortxt:"getting from local browser..."});
+    if (!navigator.geolocation) {
+      alert("navigator.geolocation is not available");
+    }
+    navigator.geolocation.getCurrentPosition(this.setPos);
+  }
+  
+  writeDataBase(){
+
+    var context = this;
+
+    db.collection("users").add({
+      first: this.state.nameinput,
+      last: "gitform",
+      lat: this.state.sLat,
+      long: this.state.sLong
+    })
+    .then(function(docRef) {
+      context.setState({errortxt : "Successfully added to database"});
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+      context.setState({errortxt: "Error adding document"});
+      console.error("Error adding document: ", error);
+    });
+
+  }
+  //
+  render() {
+    return (
+      <div className="App">
+        <Helmet>
+          <title>geo synth data collection</title>
+        </Helmet>
+        <Container className="p-3 text-left">
+          <Jumbotron >
+            <div className="bodyNoBoot">
+              <h1 className="font-weight-light">
+                geo.synth v.1.0
+              </h1>
+              <h3 className="font-weight-light">
+                A synthesiser that uses friends and families location data to create sounds.
+              </h3>
+              <p>
+                <strong>About</strong>
+                <br/><br/>
+                The geo.synth is a culmination of a few things; As an artist I have a latent compulsion to make work but during the pandemic I have a very ambiguos, dispersed, fractured 'audience', This is an attempt at an expression, a reaction to being 'locked down, to estranged relationships, to my sense of place being questioned, my sense of personal liberty being redefined..
+                <br/><br/>
+                As a musician/ tech-nerd I wanted to dive deeper into the world of music and code:  There has always been a rift between my commercial 'coder' and my personal 'artist' .  This is an attempt to heal that rift and learn more about the technologies invloved: Max/MSP, React.js, Node.js, p5.js and Firebase.
+                <br/><br/>
+                <strong>Data Collection</strong>
+                <br/><br/>
+                With this form I am attempting to collect data of users I know personally so I can develop a 'geo cluster' relative to me.  I would like to develop the idea further to include various different user 'clusters' and possibly work with the API's of other geo data apps, eg: running, cycling, walking..
+                <br/><br/>
+                If you would like to add your current lat/ long and a word to the synth please enter some text in the box and click send, the data will be held in a firebase database.  No other data is kept. If you want to preserve anonymity please use a nickname.  Please email me if you would like to remove it, or if you have any concerns about the data.
+                <br/><br/>
+                NSW 2021
+                <br/><br/>
+              </p>
+            </div>
+            <div>
+              <Form
+                onSubmit={this.handleSubmit}
+              >
+                <Form.Group 
+                  controlId="formBasicTextInput"
+                  > 
+                    <div className="bodyNoBoot border m-2 p-2">
+                      <div>
+                        Get the Lat Long Co-ordinates of this browser - only works in Chrome
+                      </div>
+                      <div className="d-flex justify-content-between">
+                        <div>
+                          <button
+                            className="btn btn-outline-secondary" 
+                            type="button"
+                            onClick={this.prepareData}
+                          >
+                            Get Co-ordinates 
+                          </button>
+                        </div>
+                        <div>
+                          {this.state.sLat} {this.state.sLong} 
+                        </div>
+                      </div>
+                    </div>
+                    <div className="bodyNoBoot border m-2 p-2">
+                      <Form.Label
+                        >Nick Name
+                      </Form.Label>
+                      <Form.Control 
+                        type="text"
+                        aria-label="name-input"
+                        size="lg"
+                        className="mb-4"
+                        name="name1input"
+                        value={this.state.nameinput}
+                        onChange={this.handleChange}
+                      />
+                      <input 
+                        type="submit" 
+                        value="Submit"
+                        disabled={!this.state.error}
+                      />
+                    </div>
+                    <div className="bodyNoBoot border m-2 p-2">
+                        {this.state.errortxt}
+                    </div>
+                </Form.Group>
+              </Form>
+            </div>
+          </Jumbotron>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
